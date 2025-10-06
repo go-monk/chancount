5ac80d8bad42b970bc595ced14328eb0764d581b
Jozef Reisinger <jozef.reisinger@gmail.com>
2025-10-06 09:40:55 +0200

    implement the elegant solution from reddit

--- cmd/example.go
+++ cmd/example.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sync/atomic"
 	"time"
 
 	"github.com/go-monk/chancount"
@@ -10,8 +12,21 @@ const n = 10
 
 func main() {
 	ch := generateInts(n)
-	ch = chancount.Elems(ch)
+
+	var count atomic.Uint64
+	ch = chancount.Elems(&count, ch)
+
+	// Print the count of received elements every second.
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for range ticker.C {
+			fmt.Printf("chan elements: %d\n", count.Load())
+		}
+	}()
+
 	processInts(ch)
+
 }
 
 func processInts(ch <-chan int) {
